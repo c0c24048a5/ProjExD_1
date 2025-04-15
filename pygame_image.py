e69c2bbfdf10de815e96687e0112df42b437e49e
Haruki-0423 <c0c24048a5@edu.teu.ac.jp>
2025-04-15 15:58:33 +0900

    残像消し

--- pygame_image.py
+++ pygame_image.py
@@ -7,21 +7,22 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 def main():
     pg.display.set_caption("はばたけ！こうかとん")
-    screen = pg.display.set_mode((800, 600))
+    screen = pg.display.set_mode((1600, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
-    tmr = 0
+    x = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [300, 200])
+        screen.blit(kk_img, [x, 200])
         pg.display.update()
-        tmr += 1        
-        clock.tick(10)
+        x += 1        
+        clock.tick(200)
+        screen.blit(bg_img, [1600, 0])
 
 
 if __name__ == "__main__":
