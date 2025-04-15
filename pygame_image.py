ae5eea1c8b42cf14e750a9a56748cdcaabed52cc
Haruki-0423 <c0c24048a5@edu.teu.ac.jp>
2025-04-15 16:35:32 +0900

    練習終わり(本当)

--- pygame_image.py
+++ pygame_image.py
@@ -7,26 +7,40 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 def main():
     pg.display.set_caption("はばたけ！こうかとん")
-    screen = pg.display.set_mode((1800, 600))
+    screen = pg.display.set_mode((1600, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
     bg2_img = pg.transform.flip(bg_img, True, False)
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
+    kk_rect = kk_img.get_rect()
+    kk_rect.center = 300, 200
+    
+
     x = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
-        screen.blit(bg_img, [0, 0])
-        screen.blit(bg2_img, [1600, 0])
-        screen.blit(bg_img, [3200, 0])
-        screen.blit(kk_img, [x, 200])
+        
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            kk_rect.move_ip((0, -10))
+        if key_lst[pg.K_DOWN]:
+            kk_rect.move_ip((0, 10))
+        if key_lst[pg.K_RIGHT]:
+            kk_rect.move_ip((10, 0))
+        if key_lst[pg.K_LEFT]:
+            kk_rect.move_ip((-10, 0))
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(bg2_img, [1600 + (-x), 0])
+        screen.blit(bg_img, [3200 + (-x), 0])
+        screen.blit(kk_img, kk_rect)
         pg.display.update()
         x += 1        
         clock.tick(200)
         if x == 3199:
             x = 0
+        
 
 
 if __name__ == "__main__":
