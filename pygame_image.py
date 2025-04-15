261b8e083b96e3de5af8ec11412d0e901bf94784
Haruki-0423 <c0c24048a5@edu.teu.ac.jp>
2025-04-15 16:01:29 +0900

    背景反転

--- pygame_image.py
+++ pygame_image.py
@@ -7,9 +7,10 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 def main():
     pg.display.set_caption("はばたけ！こうかとん")
-    screen = pg.display.set_mode((1600, 600))
+    screen = pg.display.set_mode((1800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg2_img = pg.transform.flip(bg_img, True, False)
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
     x = 0
@@ -18,11 +19,11 @@ def main():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
+        screen.blit(bg2_img, [1600, 0])
         screen.blit(kk_img, [x, 200])
         pg.display.update()
         x += 1        
         clock.tick(200)
-        screen.blit(bg_img, [1600, 0])
 
 
 if __name__ == "__main__":
